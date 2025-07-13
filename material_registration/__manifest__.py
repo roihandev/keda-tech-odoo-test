1fe5ed3e37c010827761323b066d93cceae346c3
roihandev <ahmatroihandigital@gmail.com>
2025-07-13 19:27:14 +0700

    Create __manifest__.py

---
+++ material_registration/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Material Registration',
+    'version': '1.0',
+    'summary': 'Manage materials and suppliers',
+    'description': """
+        Module for registering materials with suppliers
+    """,
+    'author': 'Your Name',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
